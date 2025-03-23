785d0649dd989be2b59737576fc2ef7d86c2967b
ThanhTuyen0302 <doancaothanhtuyen0302@gmail.com>
2025-03-23 11:33:14 +0700

    first commit

---
+++ components/user.js
@@ -0,0 +1,16 @@
+// constructor funciton
+function User(
+    name, 
+    email, 
+    avatar = "https://i.pinimg.com/736x/0f/15/7c/0f157c25cf241159b74d4863e9a4b7bd.jpg" , 
+    point = 0
+) {
+    // tạo thuộc tính (property) cho đối tượng
+    // this là từ khóa đại diện cho đối tượng được tạo ra từ constructor function
+    this.username = username;
+    this.email = email;
+    this.avatar = avatar;
+    this.point = point;
+    // tạo phương thức (method) cho đối tượng
+}
+
